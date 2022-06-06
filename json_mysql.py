9c4c2baf606d9ca42e58c99922c11100dc4ec19c
limhao <982264944@qq.com>
2022-06-06 20:31:22 +0800

    测试 测试

--- json_mysql.py
+++ json_mysql.py
@@ -9,10 +9,25 @@ db = pymysql.connect(host="localhost",
 
 )
 cursor = db.cursor()
-# 这一行是插入 信息
-sql = """INSERT INTO generic(topic,
+# # 这一行是插入 信息
+# # 启动 mysql 定义表项
+# up_mysql = """
+# CREATE TABLE myData (
+#   `main` TEXT,
+#   `topic` TEXT,
+#   `content` TEXT,
+#   `startTime` TEXT,
+#   `endTime` TEXT,
+#   `id` int(11) NOT NULL AUTO_INCREMENT,
+#   PRIMARY KEY (id)
+# );
+# """
+# cursor.execute(up_mysql)
+# db.commit()
+# db.close()
+sql = """INSERT INTO myData(main ,topic,
          content, startTime, endTime)
-         VALUES ("从Web解析到网络空间", "-Python,库之网络爬虫-Python,库之Web信息提取-Python库之Web网站开发-Python库之网络应用开发", "23.832", "34.2")"""
+         VALUES ("[10.4.1]--单元开篇.mp4", "从Web解析到网络空间", "-Python,库之网络爬虫-Python,库之Web信息提取-Python库之Web网站开发-Python库之网络应用开发", "23.832", "34.2")"""
 try:
 	cursor.execute(sql)
 	db.commit()
--- knowledgeSearch.sql
+++ knowledgeSearch.sql
@@ -17,4 +17,13 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
-DROP TABLE IF EXISTS `generic`;
+DROP TABLE IF EXISTS `myData`;
+CREATE TABLE generic (
+  `main` TEXT,
+  `topic` TEXT,
+  `content` TEXT,
+  `startTime` TEXT,
+  `endTime` TEXT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (id)
+);
